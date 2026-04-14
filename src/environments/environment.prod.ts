881b8eaa2d4f2376b42d45f13aae16c092cfc1b2
MarcosHernandez-FullStack <marcoshernandez171711@gmail.com>
2026-04-14 17:04:24 -0500

    Correccion de los environments para desarrollo y produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5016/api'
-  //apiUrl: 'https://ca-back-romo.whitedesert-ca97fbc6.eastus2.azurecontainerapps.io/api',
+  apiUrl: 'https://ca-back-romo.whitedesert-ca97fbc6.eastus2.azurecontainerapps.io/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:5016/api',
-  //apiUrl: 'https://ca-back-romo.whitedesert-ca97fbc6.eastus2.azurecontainerapps.io/api'
 };
